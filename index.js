b9af519d330243561d40fc209279b63dcf162fce
Nagaraju Kotcharlakota <klvnraju@gmail.com>
2021-01-13 18:59:08 +0000

    Run in port 80

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get("/", (req, res) => {
   console.log(`${new Date()} ${req.method} ${req.path}`);
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}/`));
