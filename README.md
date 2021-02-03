150498378634812c4c853037b9d1ecc4234c7521
Nagaraju Kotcharlakota <klvnraju@gmail.com>
2021-02-03 02:31:52 +0000

    Adding 443 and instructions for testing

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # simple-server
 
 This is a very simple (but good!) server.
+
+
+mkdir sslcert
+cd sslcert
+openssl genrsa -out key.pem
+openssl req -new -key key.pem -out csr.pem
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+rm csr.pem
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const express = require("express");
 const app = express();
 const port = 80;
+
+const privateKey = fs.readFileSync('sslcert/key.pem');
+const certificate = fs.readFileSync('sslcert/cert.pem');
+const credentials = {key: privateKey, cert: certificate};
 
 app.get("/", (req, res) => {
   console.log(`${new Date()} ${req.method} ${req.path}`);
@@ -8,3 +15,9 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}/`));
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(443, () => {
+  console.log("Example app listening on port https://localhost:443/")
+});
